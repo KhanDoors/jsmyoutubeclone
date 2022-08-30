385a6f70de2e81efb69fffc407e8ff72cc637964
syyad <syyadkhan@yahoo.com>
2022-08-30 18:19:41 -0400

    test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
 
 const App = () => (
   <BrowserRouter>
-    <Box sx={{ backgroundColor: "#000" }}>
+    <Box sx={{ backgroundColor: "red" }}>
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Feed />} />
